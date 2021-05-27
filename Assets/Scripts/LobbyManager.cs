cd71db53c3811d9a29695626ab51ae5bc411b33e
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-05-27 21:10:07 +0300

    Update LobbyManager.cs

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,10 +1,8 @@
 using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace Photon.Pun.Demo.PunBasics { 
-public class LobbyManager : MonoBehaviourPunCallbacks
+namespace Photon.Pun.Demo.PunBasics
 {
+    public class LobbyManager : MonoBehaviourPunCallbacks
+    {
         public Player[] SortedPlayers = null;
 
         private void Awake()
@@ -18,10 +16,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
         public override void OnPlayerEnteredRoom(Player other)
         {
-            SortedPlayers[PhotonNetwork.CountOfPlayers - 1] = other;
+            if (PhotonNetwork.IsMasterClient)
+            {
+                SortedPlayers[PhotonNetwork.CountOfPlayers - 1] = other;
 
-            if (PhotonNetwork.CountOfPlayers == 4)
-                photonView.RPC("SendPlayers", RpcTarget.All, SortedPlayers);
+                if (PhotonNetwork.CountOfPlayers == 4)
+                    photonView.RPC("SendPlayers", RpcTarget.All, SortedPlayers);
+            }
         }
         [PunRPC]
         void SendPlayers(Player[] players)
@@ -30,7 +31,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             if (PhotonNetwork.IsMasterClient)
                 PhotonNetwork.LoadLevel("Room For 4");
 
-            
+
 
         }
     }
