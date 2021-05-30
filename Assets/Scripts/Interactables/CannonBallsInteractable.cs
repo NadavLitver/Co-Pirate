20c8f8832bb0f7c3fc5a03b85fc86be898598c5e
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-05-30 20:49:10 +0300

    Update CannonBallsInteractable.cs

--- Assets/Scripts/Interactables/CannonBallsInteractable.cs
+++ Assets/Scripts/Interactables/CannonBallsInteractable.cs
@@ -13,20 +13,12 @@ public class CannonBallsInteractable : BaseInteractable
 
     public override void OnInteract_Start(PlayerController ctrl)
     {
-        var view = ctrl.photonView;
-        Debug.Log(view);
-        view.RPC("CollectedCannonBall", RpcTarget.All, view.Owner.GetPlayerNum());
+        ctrl.PickedUpCannonball();
+
     }
 
     public override void OnUnbecomingTarget(PlayerController ctrl)
     {
         
-    }
-
-    [PunRPC]
-    public void CollectedCannonBall(int playerNum)
-    {
-        Debug.Log(playerNum);
-        PlayerInformation.players[playerNum].playerinstance.GetComponent<PlayerController>().cannonBall.SetActive(true);
     }
 }
