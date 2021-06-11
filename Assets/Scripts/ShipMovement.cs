ef33ed17e874b494202611b0c3c35b5e8ce601d0
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-06-11 11:53:28 +0300

    Here you go

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    Vector3 center;
-
     [SerializeField, DisableInPlayMode]
     float cycleDuration = 60;
 
@@ -20,13 +18,18 @@ public class ShipMovement : MonoBehaviour
     public  Vector3 shipSpeed;
     private void Start()
     {
-        center = transform.position;
-        lastPos = center - transform.forward;
+        lastPos = transform.position - transform.forward;
         //circumference = 2 * Mathf.PI * Mathf.Sqrt((Mathf.Pow(radius.x, 2) + Mathf.Pow(radius.y, 2)) / 2);
     }
     private void Update()
     {
+        UpdatePosition();
 
+        UpdateRotation();
+    }
+
+    private void UpdatePosition()
+    {
         float cosX = Mathf.Sin((Time.time * (Mathf.PI * 2)) / cycleDuration);
         float sinZ = Mathf.Cos((Time.time * (Mathf.PI * 2)) / cycleDuration);
 
@@ -35,17 +38,18 @@ public class ShipMovement : MonoBehaviour
 
         shipSpeed = new Vector3(cosX * speedX, 0, sinZ * speedZ);
         transform.position += shipSpeed * Time.deltaTime;
+    }
+
+    private void UpdateRotation()
+    {
         Vector3 lookDir = GetLookDir();
-        
+
         Vector3 eulerRotation = Quaternion.LookRotation(lookDir).eulerAngles;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, eulerRotation.y, transform.rotation.eulerAngles.z);
         lastPos = transform.position;
     }
 
-    private Vector3 GetLookDir()
-    {
-        return transform.position - new Vector3(lastPos.x, transform.position.y, lastPos.z);
-    }
+    private Vector3 GetLookDir() => transform.position - new Vector3(lastPos.x, transform.position.y, lastPos.z);
 
     [System.Serializable, InlineProperty]
     class Range
