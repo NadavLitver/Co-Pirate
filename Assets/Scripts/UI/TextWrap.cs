4a01cc80e109a62972d3eabc0515f2306b426679
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-07-01 21:46:15 +0300

    Update TextWrap.cs

--- Assets/Scripts/UI/TextWrap.cs
+++ Assets/Scripts/UI/TextWrap.cs
@@ -9,6 +9,8 @@ public class TextWrap : MonoBehaviour
 {
     [SerializeField, LocalComponent]
     private TextMeshProUGUI _text;
+    [SerializeField, LocalComponent]
+    private DOTweenAnimation _tweenAnimation;
 
     [SerializeField]
     private string _prefix;
@@ -18,5 +20,10 @@ public class TextWrap : MonoBehaviour
 
     public void SetText(string text) => SetText<string>(text);
     public void SetText(int text) => SetText<int>(text);
-    public void SetText<T>(T text) => _text.text = _prefix + text + _suffix;
+    public void SetText<T>(T text)
+    {
+        _text.text = _prefix + text + _suffix;
+        if (_tweenAnimation != null)
+            _tweenAnimation.DORestart();
+    }
 }
