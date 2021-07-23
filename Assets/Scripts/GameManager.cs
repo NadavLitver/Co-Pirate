32a77ed462ba2ad3c2fcbb14cdfc41e6c0e6ad11
Nadav Litver <Nlitver@gmail.com>
2021-07-23 15:23:12 +0300

    leaver room fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -177,7 +177,7 @@ namespace Photon.Pun.Demo.PunBasics
         /// </summary>
         public override void OnLeftRoom()
         {
-            SceneManager.LoadScene("PunBasics-Launcher");
+            SceneManager.LoadScene("Launcher");
         }
         #endregion
 
